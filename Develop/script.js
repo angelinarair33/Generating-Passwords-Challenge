52c1fdfc6f949efc3f52f746ea4c7771bfdfd1b3
angelinarair33 <107152440+angelinarair33@users.noreply.github.com>
2022-06-27 23:35:48 -0400

    I made the prompts for lowercase and uppercase characters work with if and else statements. I also plan to apply the same concept for these to the numeric and special charac ters prompts/confirmation popups.

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 function askHowLong() {
-  let length = prompt("How many characters would you like your password to be?");
+  let length = prompt("How many characters would you like your password to be? Your choice must be a number between 8 and 128.");
   
   if (length != null) {
     confirm("You would like " + length + " characters in your password? If so, please confirm.");
@@ -11,7 +11,28 @@ function askHowLong() {
 
 // Write password to the #password input
 function generatePassword() {
-  return askHowLong(), "Kevin70$";
+  return askHowLong(), lowercaseCharacterTypes(), uppercaseCharacterTypes(), numericCharacterTypes(), specialCharacterTypes(), entirePassword();
+}
+
+function lowercaseCharacterTypes() {
+  confirm("In the following prompts, you will be asked questions about what character types you want your password to include. If you want them included, click 'OK'. If you do not want them included, click 'Cancel'.");
+  let lowerCase = confirm("Would you like to include lowercase letters in your password?");
+  
+  if (lowerCase == true) {
+    confirm("You would like lowercase characters to be included in your password? If so, please confirm.");
+  } else {
+    confirm("You do not want lowercase characters to be included in your password? On to the next option!");
+  }
+}
+
+function uppercaseCharacterTypes() {
+  let upperCase = confirm("Would you like to include uppercase letters in your password?");
+  
+  if (upperCase == true) {
+    confirm("You would like uppercase characters to be included in your password? If so, please confirm.");
+  } else {
+    confirm("You do not want uppercase characters to be included in your password? On to the next option!");
+  }
 }
 
 function writePassword() {
