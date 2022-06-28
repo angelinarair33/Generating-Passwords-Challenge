898166b14f3ad54d46b670af7e3a1689986cf592
angelinarair33 <107152440+angelinarair33@users.noreply.github.com>
2022-06-27 21:12:46 -0400

    I edited the javascript to make the same password appear in the box when you click on the generate password button.

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,15 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword() {
+  return "Kevin70$";
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
